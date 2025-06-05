4531fa7d2e13ba5b10060047b7c725cb7e111e60
Snybiux <sxirbes2@gmail.com>
2025-06-05 04:15:55 +0200

    Update listing.lua

--- Server/listing.lua
+++ Server/listing.lua
@@ -13,7 +13,7 @@ MainPart.Parent = game:GetService("CoreGui")
 MainPart.Name = "MainPart"
 
 Window.Name = "Window"
-Window.Position = UDim2.new(0, 710, 0, 40)
+Window.Position = UDim2.new(0, 740, 0, 40)
 Window.Parent = MainPart
 Window.Active = true
 Window.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
